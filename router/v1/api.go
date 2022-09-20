5a75f9dc3ddd29fe5c8fb15c7bbba655b8e8531e
Jakob Möller <contact@jakob-moeller.de>
2022-09-20 22:31:38 +0200

    expose share api

--- router/v1/api.go
+++ router/v1/api.go
@@ -17,19 +17,21 @@ func New(_ context.Context, engine *echo.Echo, config *config.Config) {
 	v1 := engine.Group("/v1") //nolint:varnamelen
 
 	{
+		middleware := authmiddleware.BasicAuthWithShare(config.Services.Accounts, config.Services.Devices)
 		auth := v1.Group("/auth")
 		registrationHandler := &RegistrationHandler{
 			config.Services.Accounts,
 			config.Services.Devices,
 		}
+		shareHandler := &ShareHandler{
+			config.Services.Accounts,
+		}
 		{
-			auth.POST("/Register", registrationHandler.Register)
+			auth.POST("/register", registrationHandler.Register)
+			auth.GET("/share", shareHandler.Share, middleware)
 		}
 
-		module := v1.Group("/module")
-		module.Use(
-			authmiddleware.BasicAuthWithShare(config.Services.Accounts, config.Services.Devices),
-		)
+		module := v1.Group("/module", middleware)
 		moduleHandler := &ModuleHandler{
 			config.Services.Modules,
 		}
