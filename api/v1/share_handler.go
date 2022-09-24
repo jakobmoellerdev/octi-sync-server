29e32759a21e2568dd118f537d4eff0d073d608e
Jakob Möller <jakob.moeller@sap.com>
2022-09-24 11:41:57 +0200

    fix wrong share user lookup

--- api/v1/share_handler.go
+++ api/v1/share_handler.go
@@ -6,16 +6,17 @@ import (
 
 	"github.com/jakob-moeller-cloud/octi-sync-server/api/v1/REST"
 	"github.com/jakob-moeller-cloud/octi-sync-server/middleware/auth"
+	"github.com/jakob-moeller-cloud/octi-sync-server/service"
 	"github.com/labstack/echo/v4"
 )
 
 func (api *API) Share(ctx echo.Context, _ REST.ShareParams) error {
-	user, found := ctx.Get(auth.UserKey).(string)
+	user, found := ctx.Get(auth.UserKey).(service.Account)
 	if !found {
 		return echo.ErrForbidden
 	}
 
-	share, err := api.Accounts.Share(ctx.Request().Context(), user)
+	share, err := api.Accounts.Share(ctx.Request().Context(), user.Username())
 	if err != nil {
 		return fmt.Errorf("error while attempting to share an account: %w", err)
 	}
